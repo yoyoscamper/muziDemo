f6fc8fc920b27603fe1ba799b039cdaac4ee0fec
yoyoscamper <530974049@qq.com>
2018-11-26 18:11:15 +0800

    Lambda测试

--- src/main/java/com/learning/springboot/controller/HelloController.java
+++ src/main/java/com/learning/springboot/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  * @Auther: 53097
@@ -14,9 +15,42 @@ import java.util.Arrays;
 @RequestMapping("/hello")
 public class HelloController {
 
+    String[] players = {"Rafael Nadal", "Novak Djokovic",
+            "Stanislas Wawrinka", "David Ferrer",
+            "Roger Federer", "Andy Murray",
+            "Tomas Berdych", "Juan Martin Del Potro",
+            "Richard Gasquet", "John Isner"};
+
+
     @RequestMapping("/hello")
     public Object hello(){
-        Arrays.asList( "a", "b", "d" ).forEach(e -> System.out.println( e ) );
-        return "hello~";
+
+        //匿名内部类排序
+        Arrays.sort(players, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return (o1.compareTo(o2));
+            }
+        });
+        System.out.println("匿名内部类排序");
+        Arrays.asList(players).forEach(e -> System.out.println( e ) );
+        System.out.println("");
+
+        //Lambda排序,根据name
+        //Comparator<String> com = (String o1,String o2) -> o1.compareTo(o2);
+        //Arrays.sort(players,com);
+        Arrays.sort(players,(String o1,String o2) -> o1.compareTo(o2));
+        System.out.println("Lambda排序");
+        Arrays.asList(players).forEach(e -> System.out.println( e ) );
+        System.out.println("");
+
+        //Lambda排序,根据字符串长度
+        Arrays.sort(players,(String o1,String o2) -> o1.length() - o2.length());
+        System.out.println("Lambda排序");
+        Arrays.asList(players).forEach(e -> System.out.println( e ) );
+        System.out.println("");
+        return "Lambda练习";
+
+
     }
 }
